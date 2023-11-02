1900f6ad3be9107702b1f0941b2167d3f1adf95d
Kenisa916 <kevnicsan@alu.edu.gva.es>
2023-11-02 12:53:10 +0100

    Ejercicio3 y 4

--- public_html/Ejercicio3.php
+++ public_html/Ejercicio3.php
@@ -2,8 +2,9 @@
 
 $cadena = "Cadena de ejemplo";
 
-echo "Longitud de la cadena: \n" ;
+echo "Longitud de la cadena: " ;
 echo strlen($cadena);
+echo "<br><br>";
 echo " Cadena al reves: ";
 echo strrev($cadena);
 
---
+++ public_html/Ejercicio4.php
@@ -0,0 +1,10 @@
+<?php
+
+$cad1 = "Esta es la cadena 1. ";
+$cad2 = "Esta es la cadena 2. ";
+$cad3 = "Esta es la cadena 3. ";
+
+echo $cad1 . $cad2 . $cad3;
+
+
+?>
