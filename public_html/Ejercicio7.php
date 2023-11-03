e20c9620a1578ab76324376c912c067c8976cdca
Kenisa916 <kevnicsan@alu.edu.gva.es>
2023-11-03 11:40:20 +0100

    Ejercicio7

---
+++ public_html/Ejercicio7.php
@@ -0,0 +1,19 @@
+<?php
+
+$num1 = rand(1, 100);
+$num2 = rand(1, 100);
+
+echo "Los numeros son $num1 y $num2<br><br>";
+
+if ($num1 > $num2){
+
+    echo "$num1 es mayor que $num2";
+
+}
+else{
+
+    echo "$num2 es mayor que $num1";
+}
+
+
+?>
