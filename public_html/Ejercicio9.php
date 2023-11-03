6eaa4be7f1a7aa8065b0f266c68dd79db300c149
Kenisa916 <kevnicsan@alu.edu.gva.es>
2023-11-03 11:50:37 +0100

    Ejercicio9

---
+++ public_html/Ejercicio9.php
@@ -0,0 +1,35 @@
+<?php
+
+$num = rand(1,7);
+
+switch ($num){
+    case 1:
+        echo "El día $num es Lunes";
+        break;
+
+    case 2:
+        echo "El día $num es Martes";
+        break;
+    case 3:
+        echo "El día $num es Miércoles";
+        break;
+
+    case 4:
+        echo "El día $num es Jueves";
+        break;
+
+    case 5:
+        echo "El día $num es Viernes";
+        break;
+
+    case 6:
+        echo "El día $num es Sábado";
+        break;
+
+    case 7:
+        echo "El día $num es Domingo";
+        break;
+}
+
+
+?>
