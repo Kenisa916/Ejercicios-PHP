a23f50d9b2cc60587fee35713df0aa034ff2c32a
Kenisa169 <kevnicsan@alu.edu.gva.es>
2023-11-16 19:26:55 +0100

    ej2

---
+++ public_html/RefuerzoFormulario/Ejercicio2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv=”Content-Type” content=”text/html; charset="UTF-8"/>
+        <title>Resultado</title>
+    </head>
+    <body style="text-align: center">
+        <h1>Resultado de la operación</h1>
+        <?php
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+            $operacion = $_POST['operacion'];
+            $resultado = $num1 $operacion $num2
+
+            print ("$resultado");
+
+        ?>
+    </body>
+</html>
