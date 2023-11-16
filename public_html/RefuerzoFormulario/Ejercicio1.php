726042eae3fceae6e8b86edd663c6863ccc1c9c4
Kenisa169 <kevnicsan@alu.edu.gva.es>
2023-11-16 19:04:24 +0100

    Ejercicio1

---
+++ public_html/RefuerzoFormulario/Ejercicio1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv=”Content-Type” content=”text/html; charset="UTF-8"/>
+        <title>Campeones de League of Legends</title>
+    </head>
+    <body>
+        <h1>Resultado de la encuesta sobre nuestro campeon</h1>
+        <?php
+            print("<h1>El formulario de PHP. Resultados del formulario</h1>");
+            $nombre = $_POST['nombre'];
+            $apellidos = $_POST['apellidos'];
+            $correo = $_POST['correo'];
+            $ciudad = $_POST['ciudad'];
+            $estudios = $_POST['favposi'];
+            $hobbies = $_POST['campeon'];
+
+            //Impresion de los datos recogidos
+            print ("<p>Formulario enviado correctamente</p>\n");
+            print ("Gracias por rellenar el formulario, $nombre, tu correo es $correo\n");
+        ?>
+    </body>
+</html>
